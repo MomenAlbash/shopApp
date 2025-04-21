a4164c734c5c1979952d0be11833ab76a95ac811
Momen Albash <Momenbash4@gmail.com>
2025-04-20 19:43:09 -0800

    create onBoardPageView

--- lib/views/onBoardingView.dart
+++ lib/views/onBoardingView.dart
@@ -7,9 +7,9 @@ class onBoardingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: onBoardBodyWidget(),
+      body: PageView.builder(
+        itemCount: 3,
+          itemBuilder:(context, index) => onBoardBodyWidget(),
       ),
     );
   }
--- lib/widgets/onBoardBodyWidgets.dart
+++ lib/widgets/onBoardBodyWidgets.dart
@@ -8,25 +8,28 @@ class onBoardBodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 600,
-          width: double.infinity,
-          child: const Image(
-            image: AssetImage('assets/images/images (8).jpg'),
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 600,
+            width: double.infinity,
+            child: const Image(
+              image: AssetImage('assets/images/images (8).jpg'),
+            ),
           ),
-        ),
-        const Text(
-          'On Board 1 Title',
-          style: TextStyle(fontSize: 24),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const Text('On Board 1 Body'),
-      ],
+          const Text(
+            'On Board 1 Title',
+            style: TextStyle(fontSize: 24),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text('On Board 1 Body'),
+        ],
+      ),
     );
   }
 }
